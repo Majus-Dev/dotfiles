270da40ac65f96c99b7bcd050a0c599648e845c6
Majus-Dev <83781075+Majus-Dev@users.noreply.github.com>
2026-09-21 20:25:23 +0200

    fix wootility possibly

---
+++ features/wootility/wootility.nix
@@ -0,0 +1,9 @@
+_: {
+  flake.nixosModules.wootility = _: {
+    services.udev.extraRules = [./wootility.rules];
+  }
+
+  flake.homeModules.wootility = {pkgs, ...}: {
+    home.packages = with pkgs; [wootility];
+  }
+}
